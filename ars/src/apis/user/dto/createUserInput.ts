2f25855f7b8e66963da612aa69342456b550af85
Arios67 <pooohoo67@gmail.com>
2022-03-16 18:50:10 +0900

    [#3]feat: 일반회원, 미대생회원 생성, 닉네임으로 유저 불러오기

---
+++ ars/src/apis/user/dto/createUserInput.ts
@@ -0,0 +1,16 @@
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class CreateUserInput {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => String)
+  phoneNum: string;
+
+  @Field(() => String)
+  email: string;
+
+  @Field(() => String)
+  nickname: string;
+}
---
+++ ars/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './apis/user/user.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    UserModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/common/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '3160',
+      database: 'ars',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+})
+export class AppModule {}
