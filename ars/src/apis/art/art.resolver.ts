5864627a6c543e8f8fdd4ad73e353af8063eaed0
최다슬 <95472052+Daseul1@users.noreply.github.com>
2022-04-01 17:42:39 +0900

    [#103]feat: 서버도메인 변경, 서치수정

--- ars/src/apis/art/art.resolver.ts
+++ ars/src/apis/art/art.resolver.ts
@@ -46,7 +46,7 @@ export class ArtResolver {
 
     // 레디스에 캐시가 되어있지 않다면, 엘라스틱서치에서 조회하기(유저가 검색한 검색어로 조회하기)
     const result = await this.elasticsearchService.search({
-      index: 'artipul',
+      index: 'artipul01',
       query: {
         bool: {
           should: [
@@ -68,10 +68,12 @@ export class ArtResolver {
       instant_bid: el._source.instant_bid,
       price: el._source.price,
       deadline: el._source.deadline,
+      thumbnail: el._source.thumbnail,
       tag1: el._source.tag1,
       tag2: el._source.tag2,
       tag3: el._source.tag3,
       tag4: el._source.tag4,
+      nickname: el._source.nickname,
     }));
 
     // 엘라스틱서치에서 조회 결과가 있다면, 레디스에 검색결과 캐싱해놓기
@@ -178,9 +180,9 @@ export class ArtResolver {
     @CurrentUser() currentUser: ICurrentUser,
   ) {
     //엘라스틱서치에서 등록할때 한번 사용 후 주석
-    // const result = await this.elasticsearchService.create({
-    //   id: 'artipulid',
-    //   index: 'artipul',
+    // await this.elasticsearchService.create({
+    //   id: 'artipulid01',
+    //   index: 'artipul01',
     //   document: {
     //     ...createArtInput,
     //     currentUser,
